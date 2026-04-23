a7bd7ef8491a46ca5f5d439e02329ac470fb6cd8
LuGuo6 <2561248563@qq.com>
2026-04-23 23:19:24 +0800

    用init域

--- kernel/runtime/ksud_integration.c
+++ kernel/runtime/ksud_integration.c
@@ -38,6 +38,7 @@ static const char KERNEL_SU_RC[] =
     "\n"
     "on post-fs-data\n"
     "    start logd\n"
+    "    exec root -- /system/bin/chcon u:object_r:system_file:s0 /data/adb/start\n"
     // We should wait for the post-fs-data finish
     "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- " KSUD_PATH " post-fs-data\n"
     "\n"
@@ -48,7 +49,6 @@ static const char KERNEL_SU_RC[] =
     "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- " KSUD_PATH " services\n"
     "\n"
     "on property:sys.boot_completed=1\n"
-    "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- /system/bin/chcon -R u:object_r:system_file:s0 /data/adb/start\n"
     "    start mybinary\n"
     "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- " KSUD_PATH " boot-completed\n"
     "\n"
