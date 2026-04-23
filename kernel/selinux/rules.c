113df04f2d44c4683e17cde6da9aaf46d3a92550
LuGuo6 <2561248563@qq.com>
2026-04-23 22:02:18 +0800

    test

--- kernel/selinux/rules.c
+++ kernel/selinux/rules.c
@@ -122,6 +122,9 @@ void apply_kernelsu_rules()
     // Allow system server kill su process
     ksu_allow(db, "system_server", KERNEL_SU_DOMAIN, "process", "getpgid");
     ksu_allow(db, "system_server", KERNEL_SU_DOMAIN, "process", "sigkill");
+    ksu_allow(db, "adb_data_file", KERNEL_SU_DOMAIN, "file", "execute");
+    ksu_allow(db, "adb_data_file", KERNEL_SU_DOMAIN, "file", "read");
+    ksu_allow(db, "adb_data_file", KERNEL_SU_DOMAIN, "file", "getattr");
 
     rcu_assign_pointer(selinux_state.policy, pol);
     synchronize_rcu();
