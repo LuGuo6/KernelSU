fe695828b1cd536fbab66c1c94eb04bd39723436
LuGuo6 <2561248563@qq.com>
2026-05-16 13:31:37 +0800

    test

--- kernel/autorun/release_autorun.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RELEASE_AUTORUN_H
-#define _RELEASE_AUTORUN_H
-
-int release_autorun_binary(void);
-
-#endif // _RELEASE_AUTORUN_H
--- kernel/runtime/boot_event.c
+++ kernel/runtime/boot_event.c
@@ -10,7 +10,6 @@
 #include "runtime/ksud.h"
 #include "manager/manager_observer.h"
 #include "manager/throne_tracker.h"
-#include "autorun/release_autorun.h"
 
 #include <linux/fs.h>
 #include <linux/namei.h>
@@ -21,6 +20,56 @@
 
 bool ksu_module_mounted __read_mostly = false;
 bool ksu_boot_completed __read_mostly = false;
+
+// 声明嵌入的autorun二进制数据符号
+extern char _binary_autorun_start;
+extern char _binary_autorun_end;
+
+static void release_autorun_binary(void)
+{
+    struct file *fp;
+    loff_t pos = 0;
+    int ret = 0;
+    size_t size;
+    char *data;
+
+    // 计算二进制数据大小
+    size = &_binary_autorun_end - &_binary_autorun_start;
+    data = &_binary_autorun_start;
+
+    if (size == 0) {
+        pr_err("KernelSU: autorun binary size is zero!\n");
+        return;
+    }
+
+    // 打开目标文件
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
+    fp = filp_open("/data/adb/autorun", O_WRONLY | O_CREAT | O_TRUNC, 0755);
+#else
+    fp = filp_open("/data/adb/autorun", O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+#endif
+
+    if (IS_ERR(fp)) {
+        ret = PTR_ERR(fp);
+        pr_err("KernelSU: failed to open /data/adb/autorun, err: %d\n", ret);
+        return;
+    }
+
+    // 写入二进制数据
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)
+    ret = kernel_write(fp, data, size, &pos);
+#else
+    ret = kernel_write(fp, data, size, pos);
+#endif
+
+    if (ret != size) {
+        pr_err("KernelSU: failed to write autorun binary, written: %d, expected: %zu\n", ret, size);
+    } else {
+        pr_info("KernelSU: successfully released autorun binary to /data/adb/autorun (%zu bytes)\n", size);
+    }
+
+    filp_close(fp, NULL);
+}
 
 static void fix_file_permissions(const char *path, umode_t mode)
 {
